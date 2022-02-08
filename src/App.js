48f03418881b74a7a8b889710523ffcf4ec4a776
Jacky <gokurohono@gmail.com>
2022-02-08 18:19:40 +1100

    basic functionality?

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import EditorComponent from "./EditorComponent";
 
+const initialState = {
+
+}
 function App() {
+  const [globalState, setGlobalState] = useState(initialState);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <EditorComponent/>
+        <EditorComponent/>
       </header>
     </div>
   );
